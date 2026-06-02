f29ef0d29377c95087a971b2da3d54e5307195ec
Dazza9905 <darendrahos@fastmail.com>
2026-06-03 00:39:58 +0200

    .

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -36,10 +36,10 @@ if status is-interactive # Commands to run in interactive sessions can go here
     set fish_greeting
 
     # Use starship
-    starship init fish | source
-    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-    end
+    # starship init fish | source
+    # if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+        # cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    # end
 
     # Aliases
     alias clear "printf '\033[2J\033[3J\033[1;1H'" # fix: kitty doesn't clear properly
